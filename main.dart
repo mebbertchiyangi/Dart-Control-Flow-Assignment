e31f62a16dd3c704dae38de69f9b15404e5add8e
Mebbert Chiyangi <>
2023-02-21 14:56:00 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+void main() {
+  print("please enter a number");
+  int number = int.parse(stdin.readLineSync()!);
+  
+     if(number > 10 ){
+    print("Your number is greater than 10");
+  }
+  
+    else if(number < 10 ){
+    print("Your number is less than 10");
+   }
+  
+   else 
+  print("Your number is equal to 10");
+  
+}
